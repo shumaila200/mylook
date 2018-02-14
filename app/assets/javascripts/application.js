9c6c93bbe7312fb4effdbb5506c2efe49977e5d2
Shumaila fatima <shumailafatima200@gmail.com>
2018-02-14 22:43:30 +0500

    complete homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= requir jquery
- //= require bootstrap-sprockets
+//= require jquery
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
